a582facf170fa01e824a593a59d0edde3ea692c9
hernan <hquintana@devosinc.com>
2016-05-03 18:04:06 -0500

    Import inicial

---
+++ app/src/main/java/pe/edu/ulima/ulimers/listadoalumnos/ListadoAlumnosActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.ulima.ulimers.listadoalumnos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.edu.ulima.ulimers.R;
+
+public class ListadoAlumnosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_alumnos);
+    }
+}
