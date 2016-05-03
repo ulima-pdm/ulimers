718647a1cfe940d6ca93aefef3e8c6871414bb97
hernan <hquintana@devosinc.com>
2016-05-03 18:25:19 -0500

    Se agregaron los metodos a las interfaces View y Presenter.

--- app/src/main/java/pe/edu/ulima/ulimers/listadoalumnos/ListadoAlumnosPresenter.java
+++ app/src/main/java/pe/edu/ulima/ulimers/listadoalumnos/ListadoAlumnosPresenter.java
@@ -1,7 +1,13 @@
 package pe.edu.ulima.ulimers.listadoalumnos;
 
+import java.util.List;
+
+import pe.edu.ulima.ulimers.beans.Alumno;
+
 /**
  * Created by hquintana on 3/05/16.
  */
 public interface ListadoAlumnosPresenter {
+    public List<Alumno> obtenerAlumnos();
+    public void aumentarPuntaje();
 }
--- app/src/main/java/pe/edu/ulima/ulimers/listadoalumnos/ListadoAlumnosView.java
+++ app/src/main/java/pe/edu/ulima/ulimers/listadoalumnos/ListadoAlumnosView.java
@@ -1,8 +1,13 @@
 package pe.edu.ulima.ulimers.listadoalumnos;
 
+import java.util.List;
+
+import pe.edu.ulima.ulimers.beans.Alumno;
+
 /**
  * Created by hquintana on 3/05/16.
  */
 public interface ListadoAlumnosView {
     public void setPresenter(ListadoAlumnosPresenter presenter);
+    public void mostrarListadoAlumnos(List<Alumno> alumnos);
 }
